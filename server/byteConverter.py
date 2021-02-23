1d77504ef085d8a720f916993c74caa690124496
Maksim P <maksim.peter777@gmail.com>
2021-02-23 16:19:15 -0500

    some initial stuff

---
+++ server/byteConverter.py
@@ -0,0 +1,15 @@
+
+
+
+
+def toRobotCoordinates(self, lat, lon):
+    pass
+
+def toRobotDirect(self, lat, lon):
+    pass
+
+def fromRobotCoordinates(self, lat, lon):
+    pass
+
+def fromRobotCoordinates(self, lat, lon):
+    pass
---
+++ server/main.py
@@ -0,0 +1,11 @@
+import server.byteConverter as _byteConverter
+from server.connectionManager import connectionManager
+
+
+
+
+
+
+#Subsystem Intilaization
+
+_connectionManager = connectionManager()
